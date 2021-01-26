ebf34260d50e8e52cf238b7ed682d0d48220084a
Shashwat Kumar <43718923+shashwat9kumar@users.noreply.github.com>
2021-01-26 17:47:18 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,7 +1,43 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = "####"
-  secret_key = "####"
+  access_key = "AKIAIM4C7XEIN2DKUWHA"
+  secret_key = "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"
+}
+
+
+
+resource "aws_vpc" "first-vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "production"
+  }
+}
+
+resource "aws_vpc" "second-vpc" {
+  cidr_block = "10.1.0.0/16"
+
+  tags = {
+    Name = "development"
+  }
+}
+
+resource "aws_subnet" "subnet-1" {
+  vpc_id     = aws_vpc.first-vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "prod-subnet"
+  }
+}
+
+resource "aws_subnet" "subnet-2" {
+  vpc_id     = aws_vpc.second-vpc.id
+  cidr_block = "10.1.1.0/24"
+
+  tags = {
+    Name = "dev-subnet"
+  }
 }
 
 
